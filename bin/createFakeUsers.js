3d8a3832c6668352a9dc5d7e58d796196969ef5f
blukat <blukatstudios>
2019-04-08 12:21:28 -0400

    fix(testing): Updated createFakeUsers to basically be an automated test. TODO: Set up real tests with a real test framework.

--- bin/createFakeUsers.js
+++ bin/createFakeUsers.js
@@ -1,10 +1,16 @@
 console.log('Creating fake users...');
 const profile = require('../profile');
 
-return profile.createProfile({
-    email: 'john@john.test',
-    username: 'john',
-    password: 'pword'
+return profile.uninstall()
+.then(function(){
+    return profile.install();
+})
+.then(function(){
+    return profile.createProfile({
+        email: 'john@john.test',
+        username: 'john',
+        password: 'pword'
+    })
 })
 .then(function(){
     return profile.updateProfileExtra(1, {
@@ -14,7 +20,20 @@ return profile.createProfile({
     });
 })
 .then(function(){
+    return profile.updateProfileExtra(1, {
+        "favorite color": "orange",
+        "favorite band": "arm band",
+        "hours spent asleep in elevator": "15"
+    });
+})
+.then(function(){
     console.log('Fake users created');
+})
+.then(function(){
+    return profile.getProfileExtra(1);
+})
+.then(function(extra){
+    console.log("Extra here " + JSON.stringify(extra));
 })
 .catch(function(error){
     console.log("oh no, an error!" + error);
