b0d66b2ca5836fd6ecdc479643a13ce02d648185
blukat <blukatstudios>
2019-04-17 12:59:39 -0400

    feat(currency): implemented most of currency module. Needs to be finished and hooked into rest of site.

--- currency.js
+++ currency.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose(),
     db = new sqlite3.Database('profile.db');
 function install(){
-    return new Promise(function(resolve, reject){
+  return new Promise(function(resolve, reject){
     let query = "";
     query += 'CREATE TABLE IF NOT EXISTS currency_balance (';
     query += 'id INTEGER PRIMARY KEY AUTOINCREMENT,';
@@ -32,12 +32,12 @@ function install(){
 
 function uninstall(){
   return new Promise(function(resolve, reject){
-      db.run("DROP TABLE IF EXISTS currency_balance", function(err, row){
-          if(err){
-              reject(err);
-          }
-          resolve(row);
-      });
+    db.run("DROP TABLE IF EXISTS currency_balance", function(err, row){
+        if(err){
+            reject(err);
+        }
+        resolve(row);
+    });
       
   })
   .then(function(){
@@ -52,31 +52,61 @@ function uninstall(){
   });
 }
 
-function createCurrency(amount, to){
-    return handleTransaction({
-        from: 0,
-        to: to,
-        amount: amount
-    });
+function createCurrency(currency, amount, to){
+  return handleTransaction({
+      from: 0,
+      to: to,
+      amount: amount,
+      currency: currency
+  });
 }
 
-function destroyCurrency(amount, from){
-    return handleTransaction({
-        from: from,
-        to: to,
-        amount: amount
-    });
+function destroyCurrency(currency, amount, from){
+  return handleTransaction({
+      from: from,
+      to: 0,
+      amount: amount,
+      currency: currency
+  });
 }
 
-function transferCurrency(amount, to, from){
-    return 1;
+function transferCurrency(currency, amount, to, from){
+  return handleTransaction({
+    from: from,
+    to: to,
+    amount: amount,
+    currency: currency
+  });
 }
 
-function handleTransaction(transaction){
-    let result = validateTransaction(trans);
-    if (!result.success) {
-        throw new Error("Could not complete transaction: " + result.errors);
-    }
+function handleTransaction(trans){
+  let result = validateTransaction(trans);
+  if (!result.success) {
+      throw new Error("Could not complete transaction: " + result.errors);
+  }
+  if (trans.from != 0) {
+    incrementBalance(trans.from, trans.currency, -trans.amount);
+  }
+  if (trans.to != 0) {
+    incrementBalance(trans.to, trans.curency, trans.amount);
+  }
+  logTransaction(trans);
+}
+
+function logTransaction(trans){
+    let query = 'INSERT into currency_transactions ';
+    query += '(from_profile, to_profile, amount, currency)';
+    query += ' values(?,?,?,?)';
+    return db.run(query, trans.from, trans.to, trans.amount, trans.currency);
+}
+
+// Use negative to subtract
+function incrementBalance(profile_id, currency, amount){
+  let query = 'UPDATE currency_balance'; 
+  query += ' SET balance = balance + ? ';
+  query += ' WHERE profile_id = ?';
+  query += ' AND currency = ?';
+  return db.run(query, amount, profile_id, currency);
 }
 
 function validateTransaction(trans){
@@ -114,5 +144,9 @@ function getBalance(profile_id, currency){
 
 module.exports = {
     install: install,
-    uninstall: uninstall
+    uninstall: uninstall,
+    createCurrency: createCurrency,
+    destroyCurrency: destroyCurrency,
+    transferCurrency: transferCurrency,
+    getBalance: getBalance
 };
